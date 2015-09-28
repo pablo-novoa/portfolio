c6322ee040132968a295a21b24b3e5c2b73db514
pablo-novoa <novoanpablo@gmail.com>
2015-09-27 23:22:48 -0300

    rotacion del cubo con menu

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var cuboZoom = false;
 $(document).ready(function(){
 	centerScene();
 	printGrilla();
@@ -8,6 +9,7 @@ $(document).ready(function(){
 		
 	});
 
+	
 	$('.main_menu').click( function(){ 
 		var thisFaceID = $(this).data('cara');
 		rotCubeMain(thisFaceID);
@@ -16,9 +18,10 @@ $(document).ready(function(){
 });
 
 function printGrilla(){
-	$('.princpal').each(function(){
+	$('.cara').each(function(){
+		var caraID = $(this).attr('id');
 		for (var i = 0; i < 100; i++) {
-			$(this).append('<div class="grilla">'+i+'</div>');
+			$(this).append('<div class="grilla">'+caraID+'</div>');
 		}
 	});
 }
@@ -53,17 +56,59 @@ function rotateCubeInit(){
 
 function rotCubeMain(face){
 	clearInterval(timerRotateCube);
-	$('#cubo').css({ transition: 'all 3s' });
-	$('#escena').css({
-		//perspective: '160vw',
-    	transform: 'scale(1)',
-    	transition: 'all 3s',
-    	top: '0'
-	});
+	var rotationValues;
 
-	$('#cubo').css({
-		transform:'rotateX(0deg) rotateY(0deg) rotateZ(0deg)'
-	});
+	switch (face) {
+	    case 1:
+	        rotationValues = 'rotateX(0deg) rotateY(0deg) rotateZ(0deg)';
+	        break;
+	    case 2:
+	        rotationValues = 'rotateX(90deg) rotateY(0deg) rotateZ(0deg)';
+	        break;
+	    case 3:
+	        rotationValues = 'rotateX(0deg) rotateY(180deg) rotateZ(0deg)';
+	        break;
+	    case 4:
+	        rotationValues = 'rotateX(-90deg) rotateY(0deg) rotateZ(0deg)';
+	        break;
+	    case 5:
+	        rotationValues = 'rotateX(0deg) rotateY(-90deg) rotateZ(0deg)';
+	        break;
+	    case 6:
+	        rotationValues = 'rotateX(0deg) rotateY(90deg) rotateZ(0deg)';
+	        break;
+	}
+
+	if(cuboZoom){
+		$('#cubo').css('-webkit-transform', 'rotateX(60deg) rotateY(45deg) rotateZ(60deg)');
+		$('#escena').css({
+	    	transform: 'scale(0.3)',
+	    	transition: 'all 1s',
+	    	top: '0'
+		});
+
+		setTimeout(function(){ 
+			$('#cubo').css({ transition: 'all 2s' });
+			$('#cubo').css('-webkit-transform', rotationValues); 
+			$('#escena').css({
+		    	transform: 'scale(1)',
+		    	transition: 'all 2s',
+		    	top: '0'
+			});
+		}, 1000);
+
+	}else{
+		$('#cubo').css({ transition: 'all 3s' });
+		$('#escena').css({
+	    	transform: 'scale(1)',
+	    	transition: 'all 3s',
+	    	top: '0'
+		});
+		$('#cubo').css('-webkit-transform', rotationValues);
+
+		cuboZoom = true;
+	}
+
 }
 
 
